d6b788d1fb1952fc2c8dbf34f4977d217a5338c8
madhouse <hinkley.marvin@gmail.com>
2016-05-14 11:13:39 -0700

    all kinds of shit

---
+++ src/character-detail/character-detail.component.ts
@@ -0,0 +1,7 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'character-details',
+})
+export class CharacterDetailsComponent {
+}
---
+++ src/character-details/character-detail.component.ts
@@ -0,0 +1,7 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'character-detail',
+})
+export class CharacterDetailComponent {
+}
